49603ce3998282807ee6a217b6819c3e8340d9dd
Mizuki297 <30870117+Mizuki297@users.noreply.github.com>
2018-02-22 17:43:45 +0700

    UI 0.0.3

--- src/Router.js
+++ src/Router.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import {Router, Stack, Scene} from 'react-native-router-flux';
 
-import Main from './component/Main';
-import Modify from './component/Modify';
-import Pay from './component/Pay';
-import Sell from './component/Sell_Items';
-import Manu from './component/Manu';
-import Add from './component/Addproduct';
-import Low from './component/Low_inventory';
-import Best from './component/Best_seller';
+import Main from './pages/Main';
+import Modify from './pages/Modify';
+import Pay from './pages/Pay';
+import Sell from './pages/Sell_Items';
+import Manu from './pages/Manu';
+import Add from './pages/Addproduct';
+import Low from './pages/Low_inventory';
+import Best from './pages/ฺฺBest_seller';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 export default class Routes extends Component {
 	render() {
 		return(
 			<Router>
 			    <Stack key="root" hideNavBar={true}>
-			      <Scene key="Main" component={Main} title="Login" initial={true}/>
+					<Scene key="Login" component={Login} title="Login" initial={true} />
+					<Scene key="Signup" component={Signup} title="Signup" />
+			      	<Scene key="main" component={Main} title="Main" />
 					<Scene key="Pay" component={Pay} title="Pay" />
 					<Scene key="Modify" component={Modify} title="Modify" />
 					<Scene key="Sell" component={Sell} title="Sell" />
